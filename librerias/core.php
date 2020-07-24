01d3196f1c9f85bcb59e4d5a9cedc5b3767225a3
3DWIN13 <ecolladocastillo@gmail.com>
2020-07-24 17:40:46 -0400

    para reportes

--- librerias/core.php
+++ librerias/core.php
@@ -7,6 +7,14 @@ function GuardarEmpleados($empleados){
     conexion::consulta($sql);
 }
 
+function GuardarDatosEntradas($datos){
+    $sql="INSERT INTO datosentradas (user, pass, admin)
+    VALUES ('{$datos->user}', '{$datos->pass}', '{$datos->admin}')";
+
+conexion::consulta($sql);
+    
+}
+
 function MostrarEmpleados(){
     $sql = "SELECT * FROM empleados";
     $rs = conexion::consulta($sql);
@@ -17,3 +25,33 @@ function MostrarEmpleados(){
     }
     return $final;
 }
+
+function Login($user){
+
+    $sql = "SELECT * FROM DatosEntradas WHERE user = '{$user}'";
+
+    $rs = conexion::consulta($sql);
+    $row = mysqli_fetch_assoc($rs);
+
+    $entrada=new stdClass();
+    $entrada->id = $row['id'];
+    $entrada->user = $row['user'];
+    $entrada->pass = $row['pass'];
+    $entrada->admin = $row['admin'];
+
+    return $entrada;
+}
+
+function BorrarEmpleados($elid){
+
+    $sql = "DELETE FROM empleados WHERE id = '{$elid}'";
+
+    conexion::consulta($sql);
+}
+
+function BorrarDatosEntradas($elid){
+    $sql = "DELETE FROM datosentradas WHERE user = '{$elid}'";
+
+    conexion::consulta($sql);
+}
+
